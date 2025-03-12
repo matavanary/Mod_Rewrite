4bfee900a1d87a121f8d9f189f2af7d3683733e6
Matavanary Studio <86429624+matavanary@users.noreply.github.com>
2025-03-12 17:01:02 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <h1>ทดสอบใช้งาน Mod Rewrite</h1>
+        <p><a href="view-1234.php">view-1234</a></p>
+        <hr>
+        <h1>ID=<?php echo $_GET['id']; ?></h1>
+<hr>
+<hr>
+
+<h1>ทดสอบใช้งาน Mod Rewrite</h1>
+        <p><a href="view/1234">view/1234</a></p>
+        <hr>
+        <h1>ID=<?php echo $_GET['id']; ?></h1>
+    </center>
+
+</body>
+</html>
